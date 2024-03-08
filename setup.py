4781fe0aaa6fb4bc2e9c66307bfce4f1d2e58297
yoo jae myeong <yoojaemyeong@gmail.com>
2024-03-08 09:01:13 +0000

    mission 1: add package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='helper',
+        version='0.0.1',
+        description='python helper functions',
+        url='github 링크',
+        author='wesleyok',
+        author_email='yoojaemyeong@gmail.com',
+        license='free',
+        packages=find_packages(),
+        install_requires=['pandas','PyQt5'],
+)
